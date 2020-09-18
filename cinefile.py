8c996ab9a6e5c4f4356251d554aa424849b585c7
lenfantterrible <mehdiat6@gmail.com>
2020-09-18 21:05:38 +0430

    Downloading Director icons is done and fine

--- cinefile.py
+++ cinefile.py
@@ -1,10 +1,13 @@
 import os, re, urllib, json, shutil
+import traceback
+
 import tmdbsimple as tmdb
+from PIL import Image
 
 tmdb.API_KEY = "6a4bc831d3389b694627785af6f5320e"
 
 
-class Movie:      # just pass file address as input, failed will be True if some problem happens
+class Movie:  # just pass file address as input, failed will be True if some problem happens
     failed = False
     search = tmdb.Search()
     name = None
@@ -22,7 +25,9 @@ class Movie:      # just pass file address as input, failed will be True if some
                     self.find_director()
                     self.failed = True if self.name is None or self.year is None or self.id is None or self.director is None \
                         else False
-                except:
+                except Exception as exc:
+                    print traceback.format_exc()
+                    print exc
                     self.failed = True
 
     def init_name(self, movie_file):
@@ -35,8 +40,8 @@ class Movie:      # just pass file address as input, failed will be True if some
 
         if findall[z] != "1080":
             self.year = int(findall[z])
-            self.name = re.split(r"\d{4}", newname)[0]
-            self.abspath = os.path.abspath(movie_file)
+            self.name = re.split(r"\d{4}", newname)[0].strip()
+            self.abspath = os.path.abspath(movie_file).strip()
 
     def find_director(self):
         self.search.movie(query=self.name)
@@ -51,14 +56,14 @@ class Movie:      # just pass file address as input, failed will be True if some
         movie = tmdb.Movies(self.id)
         for personnel in movie.credits()['crew']:
             if personnel['job'] == "Director":
-                self.director = personnel['name']
+                self.director = personnel['name'].encode("utf-8").strip()
                 break
 
     def __str__(self):
-        return  self.name + " " + str(self.year) + " : " + self.director
+        return self.name + " " + str(self.year) + " : " + self.director
 
 
-class Manager:
+class Manager:  # pass working folder path
     status = ""  # Send Status For GUI
     total_progress = 0  # Progress Bar Data for GUI
     done_progress = 0
@@ -83,8 +88,9 @@ class Manager:
         listd = os.listdir(folder)
 
         formats = ["mp4", "mkv", "avi", "flv", "avi", "wmv"]
-        try:
-            for i in listd:
+
+        for i in listd:
+            try:
                 if os.path.isdir(folder + i):
                     self.scan_folder(folder + i + os.path.sep)
                 else:
@@ -97,23 +103,28 @@ class Manager:
                         else:
                             del movie
                 self.done_progress += 1
-        except:
-            pass
+            except Exception as exc:
+                print traceback.format_exc()
+                print exc
 
     @staticmethod
-    def generate_fname(movie): # generates folder name TODO read from config how to do it
+    def generate_fname(movie):  # generates folder name TODO read from config how to do it
         return str(movie.year) + " - " + movie.name
 
-    def make_folders(self): # Should scan folder first, movie_list should not be empty
+    def make_folders(self):  # Should scan folder first, movie_list should not be empty
         work_folder = self.basefolder + os.path.sep + "CineFile"
         try:
             os.mkdir(work_folder)
-        except:
+        except Exception as exc:
+            print traceback.format_exc()
+            print exc
             self.status = "Problem working with folder"
             return
-        try:
-            for movie in self.movie_list:
-                movie_dir = work_folder + os.path.sep + movie.director + os.path.sep + Manager.generate_fname(movie)
+
+        for movie in self.movie_list:
+            movie_dir = work_folder + os.path.sep + movie.director + os.path.sep + Manager.generate_fname(movie)
+
+            try:
                 if os.path.isdir(work_folder + os.path.sep + movie.director):
                     if not os.path.isdir(movie_dir):
                         os.mkdir(movie_dir)
@@ -123,15 +134,40 @@ class Manager:
 
                 if not os.path.isfile(movie_dir + os.path.sep + os.path.basename(movie.abspath)):
                     os.rename(movie.abspath, movie_dir + os.path.sep + os.path.basename(movie.abspath))
-        except:
-            self.status = "Problem Writing the File"
+
+            except Exception as exc:
+                print traceback.format_exc()
+                print exc
+                print(movie.abspath, movie_dir + os.path.sep + os.path.basename(movie.abspath))
+                self.status = "Problem Writing the File"
 
 
-class DirectorIcon: # pass Directors folder, like CineFile folder
+class Icon:
+    desktopini = '[.ShellClassInfo] \nIconResource=icon.ico,0 \n'
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def expand2square(pil_img, background_color=0):  # copied this from note.nkmk.me
+        width, height = pil_img.size
+        if width == height:
+            return pil_img
+        elif width > height:
+            result = Image.new(pil_img.mode, (width, width), background_color)
+            result.paste(pil_img, (0, (width - height) // 2))
+            return result
+        else:
+            result = Image.new(pil_img.mode, (height, height), background_color)
+            result.paste(pil_img, ((height - width) // 2, 0))
+            return result
+
+
+class DirectorIcon:  # pass Directors folder, like CineFile folder
     status = ""
     total_progress = 0
     done_progress = 0
-    director_icons = dict() # { links folder path to icon URL }
+    director_icons = dict()  # { links folder path to jpg URL }
 
     def __init__(self, basefolder):
         self.basefolder = basefolder
@@ -143,12 +179,51 @@ class DirectorIcon: # pass Directors folder, like CineFile folder
         for item in listd:
             try:
                 self.validate_director(item)
-            except:
-                print("Problem with API")
-
+            except Exception as exc:
+                print traceback.format_exc()
+                print exc
+                self.status = "Problem with API"
 
     def validate_director(self, name):
+        if "desktop.ini" in os.listdir(self.basefolder + os.path.sep + name):
+            return
+
         search = tmdb.Search()
         person = search.person(query=name)
         if person['total_results'] != 0:
-            self.director_icons[name] = person['results'][0]['profile_path']
+            self.director_icons[self.basefolder + os.path.sep + name] = person['results'][0]['profile_path']
+            self.status = "Recognized " + name
+
+    def set_icons(self):  # should scan first, director_icons should not be empty
+        for folderpath in self.director_icons:
+            if self.director_icons[folderpath] is None:
+                continue
+
+            url = urllib.urlopen("https://image.tmdb.org/t/p/w200/" + self.director_icons[folderpath])
+            im = Image.open(url).convert('RGBA')
+            im_new = Icon.expand2square(im)
+            im_new.convert("L")
+            im_new.save(folderpath + os.path.sep + "icon.ico")
+            os.system("attrib +H \""+ folderpath + os.path.sep + "icon.ico\"")
+
+            self.set_icon(folderpath)
+
+    def set_icon(self, folderpath):
+        try:
+            with open(folderpath + os.path.sep + "desktop.ini", "w") as f:
+                f.write(Icon.desktopini)
+                f.close()
+        except Exception as exc:
+            print traceback.format_exc()
+            print exc
+            self.status = "Problem in writing desktop.ini"
+
+        try:
+            os.system('attrib +S +H "'+ folderpath + os.path.sep + 'desktop.ini"')
+            os.system('attrib +R "'+folderpath+'"')
+        except Exception as exc:
+            print traceback.format_exc()
+            print exc
+            self.status = "Problem with setting icon"
+
+
