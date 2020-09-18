1f29f0340be0891cbd41bb390948489adb1d7319
lenfantterrible <mehdiat6@gmail.com>
2020-09-18 13:24:51 +0430

    makes director and movie folders

--- cinefile.py
+++ cinefile.py
@@ -1,10 +1,10 @@
-import os, re, urllib, json
+import os, re, urllib, json, shutil
 import tmdbsimple as tmdb
 
-tmdb.API_KEY = "6a4bc831d3389b694627785af6f5320e"
+tmdb.API_KEY = ""
 
 
-class Movie:  # just pass file address as input, failed will be True if some problem happens
+class Movie:      # just pass file address as input, failed will be True if some problem happens
     failed = False
     search = tmdb.Search()
     name = None
@@ -55,7 +55,7 @@ class Movie:  # just pass file address as input, failed will be True if some pro
                 break
 
     def __str__(self):
-        return str(self.year) + "," + self.name + "," + self.director
+        return  self.name + " " + str(self.year) + " : " + self.director
 
 
 class Manager:
@@ -64,10 +64,8 @@ class Manager:
     done_progress = 0
     movie_list = list()
 
-    def __init__(self, basefolder, outfolder):
-        self.outfolder = outfolder
-        self.folder = basefolder
-        self.total_progress = Manager.count_progress(basefolder)
+    def __init__(self, basefolder):
+        self.basefolder = basefolder
 
     @staticmethod
     def count_progress(folder):
@@ -79,7 +77,8 @@ class Manager:
         return total_progress
 
     def scan_folder(self, folder=None):  # check self.movie_list afterwards!
-        folder = self.folder if folder is None else folder
+        self.total_progress = Manager.count_progress(self.basefolder)
+        folder = self.basefolder if folder is None else folder
         folder = folder + os.path.sep
         listd = os.listdir(folder)
 
@@ -93,13 +92,42 @@ class Manager:
                         movie = Movie(folder + i)
                         if not movie.failed:
                             self.status = "Recognized " + str(movie)
+                            print(self.status)
                             self.movie_list.append(movie)
                         else:
                             del movie
+                self.done_progress += 1
         except:
             pass
 
+    @staticmethod
+    def generate_fname(movie): # generates folder name TODO read from config how to do it
+        return str(movie.year) + " - " + movie.name
 
-m = Manager("/media/mehdi/Mehdi")
+    def make_folders(self): # Should scan folder first, movie_list should not be empty
+        work_folder = self.basefolder + os.path.sep + "CineFile"
+        try:
+            os.mkdir(work_folder)
+        except:
+            self.status = "Problem working with folder"
+            return
+        try:
+            for movie in self.movie_list:
+                movie_dir = work_folder + os.path.sep + movie.director + os.path.sep + Manager.generate_fname(movie)
+                if os.path.isdir(work_folder + os.path.sep + movie.director):
+                    if not os.path.isdir(movie_dir):
+                        os.mkdir(movie_dir)
+                else:
+                    os.mkdir(work_folder + os.path.sep + movie.director)
+                    os.mkdir(movie_dir)
+
+                if not os.path.isfile(movie_dir + os.path.sep + os.path.basename(movie.abspath)):
+                    os.rename(movie.abspath, movie_dir + os.path.sep + os.path.basename(movie.abspath))
+        except:
+            self.status = "Problem Writing the File"
+
+
+
+m = Manager("/media/mehdi/Mehdi/Hard")
 m.scan_folder()
-print(str(m.movie_list))
+m.make_folders()
